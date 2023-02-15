a60bd4e17f8e145978bb5e42967bba51c0ea5ac8
JoseViewture <110395155+JoseViewture@users.noreply.github.com>
2023-02-15 22:50:23 +0000

    initial commit

---
+++ Models/Reading.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElectricityUsageDashboard.Models
+{
+    public class Reading
+    {
+        public int ReadingId { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MMM-dd-yy}")]
+        public DateTime ReadingDate { get; set; } 
+        public int CurrentReading { get; set; }
+        public bool IsBilledReading { get; set; }
+        
+
+    }
+}
