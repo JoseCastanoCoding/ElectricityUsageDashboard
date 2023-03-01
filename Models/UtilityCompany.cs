af8f2d953bed50e2a5cb5e70889abee7b9548ecc
JoseViewture <110395155+JoseViewture@users.noreply.github.com>
2023-03-01 23:05:36 +0000

    UtilityCompany class created

---
+++ Models/UtilityCompany.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElectricityUsageDashboard.Models
+{
+    public class UtilityCompany
+    {
+        [Key]
+        public int CompanyID { get; set; }
+        public string CompanyName { get; set;}
+        public string CompanyPhoneNumber { get; set;}
+        public string CompanyEmail { get; set;}
+        public string CompanyAddress { get; set;}
+        public string CompanyCity { get; set;}
+        public string CompanyRegion { get; set;}
+        public string CompanyPostalCode { get; set;}
+        public User AccountNumber { get; }
+        public User InvoiceNumber { get; }
+        public Reading InvoiceDate { get; }
+    }
+}
